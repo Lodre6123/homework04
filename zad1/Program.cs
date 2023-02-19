2c398913dd53cc8dae8321815961ac4be0af13b5
Lodre6123 <nikpash6123@mail.ru>
2023-02-19 17:17:02 +0300

    g1

---
+++ zad1/Program.cs
@@ -0,0 +1,20 @@
+﻿/*Напишите цикл, который принимает на вход два числа (A и B) 
+и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16
+*/
+Console.Write("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+int ExpInt(int num1, int num2)
+{
+int result = 1;
+    for(int i=1; i <= num2; i++)
+    {
+    result = result * num1;
+    }
+    return result;
+}
+int expN = ExpInt(num1, num2);
+Console.WriteLine("Выводим: " + expN);
