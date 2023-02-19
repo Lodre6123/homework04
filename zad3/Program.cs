36ba36c67f0983e345c6aa157adbff195649f8bb
Lodre6123 <nikpash6123@mail.ru>
2023-02-19 17:31:55 +0300

    array g

---
+++ zad3/Program.cs
@@ -0,0 +1,27 @@
+﻿/* Напишите программу, которая задаёт массив из m элементов и выводит их на экран.
+
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33]
+*/
+int[] CrArray(int size)
+{
+    int[] array = new int[size];
+    for(int i = 0; i < size; i++)
+    {
+        Console.Write($"Напишите массив[{i}]: ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+
+void PrArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+    Console.Write("\n");
+}
+
+Console.Write("Введите размер массива: ");
+int arraySize = Convert.ToInt32(Console.ReadLine());
+int[] array = CrArray(arraySize);
+PrArray(array);
